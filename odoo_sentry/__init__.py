c7c59afc83ba54861aca33e6338ee3b9c53f5254
Naglis Jonaitis <naglis@hbee.eu>
2015-11-19 17:54:15 +0200

    [ADD] Credit

--- odoo_sentry/__init__.py
+++ odoo_sentry/__init__.py
@@ -2,7 +2,8 @@
 ##############################################################################
 #
 #    Odoo - Sentry connector
-#    Copyright (C) 2014 Mohammed Barsi.
+#    Copyright (C) 2014 Mohammed Barsi
+#    Copyright (C) 2015 Naglis Jonaitis
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
